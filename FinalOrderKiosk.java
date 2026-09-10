55fc87906a841dac0764c117ecdf2cb177e60d02
SeouliteParker <steeldaily@gmail.com>
2026-09-10 11:35:16 +0900

    file name update

--- FinalOrderKiosk.java
+++ FinalOrderKiosk.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FinalOrderKiosk_student {
+public class FinalOrderKiosk{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
